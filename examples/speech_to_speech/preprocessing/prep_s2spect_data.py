e7521572ec32c86177732e074d787b38a92025c1
TruongScotl <53056290+TruongScotl@users.noreply.github.com>
2023-11-22 11:16:45 +0700

    Add files via upload

--- examples/speech_to_speech/preprocessing/prep_s2spect_data.py
+++ examples/speech_to_speech/preprocessing/prep_s2spect_data.py
@@ -10,7 +10,6 @@ import os
 from pathlib import Path
 import shutil
 import torchaudio
-from numba import jit
 
 import soundfile as sf
 from tqdm import tqdm
@@ -26,7 +25,7 @@ logger = logging.getLogger(__name__)
 
 MANIFEST_COLUMNS = ["id", "src_audio", "src_n_frames", "tgt_audio", "tgt_n_frames"]
 
-@jit
+
 def prepare_target_data(args, tgt_audios):
     feature_name = "logmelspec80"
     zip_path = args.output_root / f"{feature_name}.zip"
@@ -57,7 +56,7 @@ def prepare_target_data(args, tgt_audios):
 
     return zip_path
 
-@jit
+
 def process(args):
     os.makedirs(args.output_root, exist_ok=True)
 
